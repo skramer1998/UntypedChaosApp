edf7186099ece72c3abdba6ffe0f3bcbaac9e07f
Tyler <tylerdng18@gmail.com>
2019-04-04 10:47:11 -0500

    Setting up Tests

--- FSA/tests.py
+++ FSA/tests.py
@@ -45,7 +45,39 @@ class TestAccount(TestCase):
         self.assertEqual(self.a1.userEmail, "Natedog@gmail.com")
         self.assertEqual(self.a1.userPhone, 1010101010)
         self.assertEqual(self.a1.userAddress, "The Trap House")
+        self.a1.editSelf("Nate", self.T, "glasses@gmail.com", "22 Middleton Rd", 1234567890)
         # checks if editSelf worked correctly with correct parameters
+
+        self.assertFalse(
+            self.a1.editOther(self.a1.user, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.assertFalse(
+            self.a1.editOther(self.a1.user, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.assertTrue(
+            self.a1.editOther(self.a1.user, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.assertTrue(
+            self.a1.editOther(self.a1.user, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.a1.editSelf("Nate", self.T, "glasses@gmail.com", "22 Middleton Rd", 1234567890)
+        # checks only accounts with proper ID can edit other accounts
+
+        self.assertFalse(self.a4.editOther(self.a1))
+        self.assertFalse(self.a4.editOther(self.a1, "Howdy yall, I'm Towly!"))
+        self.assertFalse(self.a4.editOther(self.a1, 'a', self.I, "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.assertFalse(self.a4.editOther(self.a1, "NateDog", "iNstRectAr", "Natedog@gmail.com", "The Trap House", 1010101010))
+        self.assertFalse(self.a4.editOther(self.a1, "NateDog", self.I, "Natedog@gmail.com", 42, 1010101010))
+        self.assertFalse(self.a4.editOther(self.a1, "NateDog", self.I, "NAT@", "The Trap House", 1010101010))
+        self.assertFalse(self.a4.editOther(self.a1, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1))
+        # returns false for bad parameters given
+
+        self.a4.editOther(self.a1, "NateDog", self.I, "Natedog@gmail.com", "The Trap House", 1010101010)
+        self.assertEqual(self.a1.user.username, "NateDog")
+        self.assertEqual(self.a1.user.userEmail, "Natedog@gmail.com")
+        self.assertEqual(self.a1.userName, "NateDog")
+        self.assertEqual(self.a1.userID, self.I)
+        self.assertEqual(self.a1.userEmail, "Natedog@gmail.com")
+        self.assertEqual(self.a1.userPhone, 1010101010)
+        self.assertEqual(self.a1.userAddress, "The Trap House")
+        self.a1.editSelf("Nate", self.T, "glasses@gmail.com", "22 Middleton Rd", 1234567890)
+        # checks edit other successfully edits user info with correct parameters
 
         self.assertTrue(self.a1.is_TA() | self.a1.is_Instructor() | self.a1.is_Admin() | self.a1.is_Supervisor())
         self.assertFalse(self.a1.is_TA() & self.a1.is_Instructor())
@@ -56,7 +88,16 @@ class TestAccount(TestCase):
         self.assertFalse(self.a1.is_Admin() & self.a1.is_Supervisor())
         # makes ure user has an ID, and that it does not have more than 1 ID.
 
+        self.assertFalse(self.a1.invalidatePassword(self.a1.user))
+        self.assertFalse(self.a2.invalidatePassword(self.a1.user))
+        self.assertTrue(self.a3.invalidatePassword(self.a1.user))
+        self.assertTrue(self.a4.invalidatePassword(self.a1.user))
+        # checks only accounts with proper ID can invalidate passwords
 
-
+        self.assertFalse(self.a1.createCourse())
+        self.assertFalse(self.a2.createCourse())
+        self.assertTrue(self.a3.createCourse())
+        self.assertTrue(self.a4.createCourse())
+        # checks only accounts with proper ID can create courses
 
 
