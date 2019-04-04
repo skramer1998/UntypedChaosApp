5bdad8e88d1ea20284a7346f5e88ddf108290576
Tyler <tylerdng18@gmail.com>
2019-04-04 09:20:38 -0500

    Initial Commit

--- FSA/models.py
+++ FSA/models.py
@@ -10,7 +10,6 @@ class MyModel(models.Model):
     fieldTwo = models.IntegerField(default=0)
     color = models.CharField(max_length=7)
 
-
 class AccountModel(models.Model):
     role = models.CharField(max_length=12)
     name = models.CharField(max_length=30)
