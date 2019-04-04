cfe1a4fdf31ab028ffe3395907fc156b89c12666
skramer1998 <swkramer@uwm.edu>
2019-04-04 10:08:56 -0500

    basic command line parsing complete

--- FSA/models.py
+++ FSA/models.py
@@ -29,7 +29,6 @@ class Terminal(models.Model):
             # if incorrect, print "wrong password" and end the function call
             password = getpass.getpass()
 
-
     def logout(self):
         if self.user is None:
             print("you aren't logged in, so you can't log out")
@@ -38,6 +37,7 @@ class Terminal(models.Model):
             user = None
             print("logged out")
             return
+
 
 class MyModel(models.Model):
     fieldOne = models.CharField(max_length=20)
@@ -54,6 +54,7 @@ class AccountModel(models.Model):
     userName = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 """
+
 
 class CoursesModel(models.Model):
     name = models.CharField(max_length=30)
