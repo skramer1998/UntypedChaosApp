4b7f715115cb67ca84cfb57e43585fa05943e269
Nate <ndkohls@uwm.edu>
2019-04-04 09:10:56 -0500

    copying (and slightly editing) the accounts stuff

--- FSA/models.py
+++ FSA/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -33,10 +34,135 @@ class CoursesModel(models.Model):
     labs = models.IntegerField(default=0)
 
 
-class UserModel(models.Model):
-    is_TA = models.BooleanField('TA status', default=False)
-    is_Instructor = models.BooleanField('Instructor status', default=False)
-    is_Supervisor = models.BooleanField('Supervisor status', default=False)
-    is_Admin = models.BooleanField('Admin status', default=False)
-# this is how permissions will be handled. This will let us use the same account with multiple permission levels
-# it's also less work than user groups, but I can edit this to include them if needed
+class Account(models.Model):
+    def __init__(self, id, first, middle, last, email, phone, address):
+        self.userID = id
+        self.userName = first + " " + middle + " " + last
+        self.userEmail = email
+        self.userPhone = phone
+        self.userAddress = address
+        self.user = User.objects.create_user(self.userID, self.userEmail)
+        self.user.first_name = first
+        self.user.last_name = last
+        self.user.groups = None
+        self.user.save()
+        # create an account in the user DB. No password at first, default permissions for user are none, individual
+        # group status should be set in a later step.
+
+        # should return false if not set otherwise true
+
+    def editPassword(self, newPassword):
+        # Validate permissions before being able to call this function
+        print("confirm new password: ")
+        if newPassword == input():
+            if not self.user.has_usable_password():
+                self.user.set_password(newPassword)
+                self.user.save()
+                print("new password set")
+            else:
+                print("type current password for " + self.user.username)
+                if self.user.check_password(input()):
+                    self.user.set_password(newPassword)
+                    self.user.save()
+                    print("new password set")
+                else:
+                    print("wrong password for " + self.user.username)
+        else:
+            print("the new passwords don't match, start again from the begining.")
+
+    def editSelf(self, name, id, email, phone, address):
+        self.user.username = name
+        self.user.userEmail = email
+        self.userName = name
+        self.userID = id
+        self.userEmail = email
+        self.userPhone = phone
+        self.userAddress = address
+        self.user.save()
+        # should return false if not set otherwise true
+
+    def is_TA(self, user):
+        return user.user.groups.filter(name='TA').exists()
+
+    def is_Instructor(self, user):
+        return user.user.groups.filter(name='Instructor').exists()
+
+    def is_Admin(self, user):
+        return user.user.groups.filter(name='Admin').exists()
+
+    def is_Supervisor(self, user):
+        return user.user.groups.filter(name='Supervisor').exists()
+
+    def grantGroupStatus(self, user, groupName):  # supervisors and admins can grant permissions
+        if Account.is_Supervisor(self):
+            if groupName == "TA" or groupName == "Instructor" or groupName == "Admin" or groupName == "Supervisor":
+                user.user.group.add(groupName)
+                user.user.save()
+                print(user.userName + " has been added to "+ groupName)
+                return True
+            else:
+                print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
+                return False
+        elif Account.is_Admin(self):
+            if groupName == "TA" or groupName == "Instructor" or groupName == "Admin":
+                user.user.group.add(groupName)
+                user.user.save()
+                print(user.userName + " has been added to "+ groupName)
+                return True
+            elif groupName == "Supervisor":
+                print("Admin accounts cannot add Supervisor status")
+                return False
+            else:
+                print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
+                return False
+        else:
+            print("sign in as an Admin or Supervisor to grant group assignments")
+
+    def invalidatePassword(self, user):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            user.user.setUnusablePassword()
+            user.user.save()
+            return True
+        else:
+            print("only Admins and Supervisors can invalidate passwords")
+            return False
+
+    def editOther(self, user, name, id, email, phone, address):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            user.user.username = name
+            user.user.userEmail = email
+            user.userName = name
+            user.userID = id
+            user.userEmail = email
+            user.userPhone = phone
+            user.userAddress = address
+            user.user.save()
+            print("changes made")
+            return True
+        else:
+            print("only Admins and Supervisors can edit the account details of others")
+            return False
+        # should return false if not set otherwise true
+
+    def createCourse(self, otherargs):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            # call course constructor
+            return True
+        else:
+            print("Only Admins and Supervisors can add new Courses")
+            return False
+
+    def modifyCouse(self, course, otherargs):
+        return False
+        # pass shit to course edit method, if self has the permissions to do so.
+
+    def publicInfo(self, user):
+        return 0
+        # big old string with whatever is actually public
+        # can include a permissions check ala other methods in this class
+
+    def toString(self):
+        return "User ID: "+self.userID+"\nUsername: "+self.user.username+"\nUserEmail: " + \
+               self.user.userEmail+"\nUserPhone: "+self.userPhone+"\nUser Address: "+self.userAddress
+
+
