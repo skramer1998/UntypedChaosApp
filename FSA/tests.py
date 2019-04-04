9de27b2ff3ad84af8c0eb4a80f219f32493b4204
Tyler <tylerdng18@gmail.com>
2019-04-04 09:44:03 -0500

    Setting up Tests

--- FSA/tests.py
+++ FSA/tests.py
@@ -10,13 +10,13 @@ class TestAccount(TestCase):
         self.s = "Supervisor"
 
         self.a1 = Account.objects.create(userID = self.t, userName = "Nate", userEmail = "glasses@gmail.com",
-                                         userAddress = "22 Middleton rd")
+                                         userAddress = "22 Middleton rd", userPhone = 1234567890)
         self.a2 = Account.objects.create(userID=self.i, userName="Andres", userEmail="sweatshirt@gmail.com",
-                                         userAddress="34 Giannis rd")
+                                         userAddress="34 Giannis rd", userPhone = 2121212121)
         self.a3 = Account.objects.create(userID=self.a, userName="Sean", userEmail="tshirt@gmail.com",
-                                         userAddress="13 Brogdon ave")
+                                         userAddress="13 Brogdon ave", userPhone = 4145554444)
         self.a4 = Account.objects.create(userID=self.s, userName="Tyler", userEmail="jersey@gmail.com",
-                                         userAddress="11 Lopez ct")
+                                         userAddress="11 Lopez ct", userPhone = 4207106969)
 
     def test_editAccount(self):
 
