4f7234d2e92e2a39f232c614432eb693bfa0e4fc
andresmunoz <munoz24@uwm.edu>
2019-04-08 22:46:16 -0500

    commented off some stuff from account. implemented a groupid system to account. updated terminal to work with changes.

--- FSA/models.py
+++ FSA/models.py
@@ -10,7 +10,6 @@ user = None
 
 
 class Terminal(models.Model):
-
     """
     Terminal Class: The class used to make the webpage command line work correctly
     """
@@ -18,6 +17,7 @@ class Terminal(models.Model):
     """
     Function called by the Home view, immediately passes input to the command parser.
     """
+
     def command(self, input_string):
         command_string = input_string
         return self.parseCommand(command_string)
@@ -28,6 +28,7 @@ class Terminal(models.Model):
     they are already logged in.  IF they are not tell them to login.  IF they are, continue on processing
     the parsed command.
     """
+
     def parseCommand(self, cmdStr):
         global user
         parseCmd = cmdStr.split()
@@ -39,14 +40,14 @@ class Terminal(models.Model):
             else:
                 return "You need to provide login arguments!"
         elif parseCmd[0].lower() == 'createaccount':
-            if len(parseCmd) > 9:
+            if len(parseCmd) > 10:
                 #  print("branch 1")
                 return self.createaccount(parseCmd[1], "" + parseCmd[2] + " " + parseCmd[3] + " " + parseCmd[4],
-                                          parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8], parseCmd[9])
-            elif len(parseCmd) > 8:
+                                          parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8], parseCmd[9], parseCmd[10])
+            elif len(parseCmd) > 9:
                 #  print("branch 2")
                 return self.createaccount(parseCmd[1], "" + parseCmd[2] + " " + parseCmd[3], parseCmd[4],
-                                          parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8])
+                                          parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8], parseCmd[9])
             else:
                 return "not enough args to create account"
         else:
@@ -54,12 +55,14 @@ class Terminal(models.Model):
                 return "You are not logged in, you must login before entering commands."
             elif parseCmd[0].lower() == 'accountlist':
                 return self.accountList()
+            elif parseCmd[0].lower() == 'courselist':
+                return self.courseList()
             elif parseCmd[0].lower() == 'logout':
                 return self.logout()
             elif parseCmd[0].lower() == 'createcourse':
-                if len(parseCmd) > 9:
+                if len(parseCmd) > 8:
                     return self.createCourse(parseCmd[1], parseCmd[2], parseCmd[3], parseCmd[4],
-                                             parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8], parseCmd[9])
+                                             parseCmd[5], parseCmd[6], parseCmd[7], parseCmd[8])
                 else:
                     return "not enough args to create a new course"
             else:
@@ -68,13 +71,18 @@ class Terminal(models.Model):
     """
     Print out the list of user accounts by userName.
     """
+
     def accountList(self):
         return "Account List: " + ", ".join(list(Account.objects.values_list('userName', flat=True)))
+
+    def courseList(self):
+        return "Course List: " + ", ".join(list(Course.objects.values_list('name', flat=True)))
 
     """
     Create the password for new accounts. This is an internal function, not to be called directly.
     """
-    def setNewPassword(self, npuser, passwordAttempt1, passwordAttempt2 ):
+
+    def setNewPassword(self, npuser, passwordAttempt1, passwordAttempt2):
         global user
         if not npuser.has_usable_password():
             while True:
@@ -93,27 +101,30 @@ class Terminal(models.Model):
     """
     A list of all commands, what they do, and how to use them.
     """
+
     def help(self):
         return "Commands: \n----------------\
               \n\nlogin-- sign into an existing account.\nusage: login Username Password\
               \n\n\logout-- sign out from your account\nusage: logout\
-              \n\ncreateAccount-- makes a new account, default permissions none.\nusage: createAccount SignInName\
+              \n\ncreateaccount-- makes a new account, default permissions none.\nusage: createAccount SignInName\
               FirstName [MiddleName(optional)] LastName Email Phone Address NewAccountpassword NewAccountpassword\
-              \n\naccountList-- returns a list of all accounts\nusage: accountList\
-              \n\ncreateCourse-- creates a new course\n\usage: createCourse name number place days time semester\
+              \n\naccountlist-- returns a list of all accounts\nusage: accountList\
+              \n\ncreatecourse-- creates a new course\n\usage: createCourse name number place days time semester\
               professor ta #OfLabs\
-              \n\nlistCourses-- lists all courses\nusage: listCourses"
+              \n\ncourselist-- lists all courses\nusage: listCourses"
 
     """
     Create a new account.
     """
-    def createaccount(self, SignInName, name, email, phone, address, password1, password2):
-        Account.create(SignInName, name, email, phone, address, password1, password2)
+
+    def createaccount(self, SignInName, name, email, phone, address, password1, password2, groupid):
+        Account.create(SignInName, name, email, phone, address, password1, password2, groupid)
         return "Your account was successfully created! You were also signed in."
 
     """
     Login to an existing account.
     """
+
     def login(self, username, password):
         global user
         if user is not None:
@@ -135,6 +146,7 @@ class Terminal(models.Model):
     """
     Logout of currently logged in account, if possible.
     """
+
     def logout(self):
         global user
         if user is None:
@@ -146,106 +158,11 @@ class Terminal(models.Model):
     """
     Create a course using the given information
     """
-    def createCourse(self, name, number, place, days, time, semester, professor, ta, labs):
-        return Course.create(name, number, place, days, time, semester, professor, ta, labs)
 
-
-class Course(models.Model):
-
-    """
-    Courses Class: The class used to store course objects / edit / create them
-    """
-
-    # need to change later. Lab class? courses can have more than one lab and more than one ta
-    name = models.CharField(max_length=30)
-    number = models.IntegerField(default=0)
-    place = models.CharField(max_length=30)
-    days = models.CharField(max_length=30)
-    time = models.CharField(max_length=30)
-    semester = models.CharField(max_length=30)
-    professor = models.CharField(max_length=30)
-    ta = models.CharField(max_length=30)
-    labs = models.IntegerField(default=0)
-
-    @classmethod
-    def create(cls, name, number, place, days, time, semester, professor, ta, labs):
-        print("got into course.create")
-        if Course.objects.filter(name__contains=name).first() is not None:
-            return "Course already exist"
-        else:
-            print("got into the else")
-            newThing = Course.cls(Course(Course), name=name, number=number, place=place, days=days, time=time, semester=semester, professor=professor,
-                                  ta=ta, labs=labs)
-            print(len(Course.objects.all()))
-            return "Course was created"
-
-    def cls(self, name, number, place, days, time, semester, professor, ta, labs):
-        print("got into cls")
-        almost_course = Course.objects.create(name=name, number=number, place=place, days=days,
-                                              time=time, semester=semester, professor=professor,
-                                              ta=ta, labs=labs)
-        """self.name = name
-        self.number = number
-        self.place = place
-        self.days = days
-        self.time = time
-        self.semester = semester
-        self.professor = professor
-        self.ta = ta
-        self.labs = labs"""
-        print(almost_course)
-        almost_course.save()
-        return almost_course
-
-    def search(self, name):
-        if Course.objects.filter(name__contains=name).first() is not None:
-            return True
-        else:
-            return False
-
-    def setname(self, new_name):
-        self.name = new_name
-
-    def setnumber(self, new_number):
-        self.number = new_number
-
-    def setplace(self, new_place):
-        self.place = new_place
-
-    def setdays(self, new_days):
-        self.days = new_days
-
-    def settime(self, new_time):
-        self.time = new_time
-
-    def setsemester(self, new_semester):
-        self.semester = new_semester
-
-    def setprofessor(self, new_professor):
-        self.professor = new_professor
-
-    def setta(self, new_ta):
-        self.ta = new_ta
-
-    def setlabs(self, new_labs):
-        self.labs = new_labs
-
-
-    # convert to string one course
-    def tostr(self):
-        return self.name + " " + self.number + " " + self.place + " " + self.days + " " + self.time + " " + self.semester + " " + self.professor + " " + self.ta + "" + self.labs
-
-    # call Course.objects.all() to get all courses to string
-    def __str__(self):
-        return self.name + " " + self.number + " " + self.place + " " + self.days + " " + self.time + " " + self.semester + " " + self.professor + " " + self.ta + "" + self.labs
-
-    def listCourses(self):
-        return tostr(Course.objects.values_list)
-    #probs doesn't work but I wrote a line of code so here ya go
-
+    def createCourse(self, name, number, place, days, time, semester, professor, ta):
+        return Course.create(name, number, place, days, time, semester, professor, ta)
 
 class Account(models.Model):
-
     """
     Account Class: The class we're using to store account objects / edit / create them.
     """
@@ -256,15 +173,18 @@ class Account(models.Model):
     userPhone = models.CharField(max_length=30)
     userAddress = models.CharField(max_length=120)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Associated Auth User+")
+    groupid = models.IntegerField(default=0)
+    """1=SU, 2=AD, 3=IN, 4=TA"""
 
     """
     Overwrite the generic Account str() function for printing
     """
+
     def __str__(self):
-        return str(self.SignInName) + " " + str(self.userName) + " " + str(self.userEmail)
+        return str(self.SignInName)
 
     @classmethod
-    def create(cls, userid, username, email, phone, address, password1, password2):
+    def create(cls, userid, username, email, phone, address, password1, password2, id):
         print("go t into create")
         '''x = username.split()
         if len(x) == 2:
@@ -272,19 +192,19 @@ class Account(models.Model):
         else:
               return cls(SignInName=userid, first=x[0], middle=x[1], last=x[2], email=email, phone=phone, address=address)
         '''
-        return Account.cls(Account(cls), userid, username, email, phone, address, password1, password2)
+        return Account.cls(Account(cls), userid, username, email, phone, address, password1, password2, id)
 
-    def cls(self, othernameforid, username, email, userPhone, address, password1, password2):
+    def cls(self, othernameforid, username, email, userPhone, address, password1, password2, id):
         if Account.objects.filter(SignInName=othernameforid).first() is not None:
             return "That username is already in use, please select a different one."
-            #return self
+            # return self
         print("ayyo let's create some shit")
         print(hash(othernameforid))
         user = User.objects.create_user(username=othernameforid, email=email)  # '''id=len(User.objects.all())+1,'''
         account = Account.objects.create(SignInName=othernameforid,
                                          userName=username, userEmail=email, userPhone=userPhone, userAddress=address,
-                                         user_id=user.id)
-        #user_id=hash(othernameforid)
+                                         user_id=user.id, groupid=id)
+        # user_id=hash(othernameforid)
         #  user_id cannot be trusted to set itself. creating an accout where the username hashes to the same value as
         #  an existing account will fail.
         terminal = Terminal()
@@ -337,25 +257,19 @@ class Account(models.Model):
         self.user.save()
         # should return false if not set otherwise true
 
-    def is_TA(self, search_user):
-        return search_user.user.groups.filter(name='ta').exists()
+    def getid(self):
+        return self.groupid
 
-    def is_Instructor(self, search_user):
-        return search_user.user.groups.filter(name='instructor').exists()
 
-    def is_Admin(self, user):
-        return user.user.groups.filter(name='admin').exists()
 
-    def is_Supervisor(self, user):
-        return user.user.groups.filter(name='supervisor').exists()
-
+"""
     def grantGroupStatus(self, user, groupName):  # supervisors and admins can grant permissions
         groupName = groupName.lower()
         if Account.is_Supervisor(self):
             if groupName == "ta" or groupName == "instructor" or groupName == "admin" or groupName == "supervisor":
                 user.user.group.add(groupName)
                 user.user.save()
-                print(user.userName + " has been added to "+ groupName +" role")
+                print(user.userName + " has been added to " + groupName + " role")
                 return True
             else:
                 print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
@@ -364,7 +278,7 @@ class Account(models.Model):
             if groupName == "ta" or groupName == "instructor" or groupName == "admin":
                 user.user.group.add(groupName)
                 user.user.save()
-                print(user.userName + " has been added to "+ groupName)
+                print(user.userName + " has been added to " + groupName)
                 return True
             elif groupName == "supervisor":
                 print("Admin accounts cannot add Supervisor status")
@@ -400,7 +314,7 @@ class Account(models.Model):
             print("only Admins and Supervisors can edit the account details of others")
             return False
         # should return false if not set otherwise true
-
+    
     def createCourse(self, otherargs):
         if Account.is_Admin(self) or Account.is_Supervisor(self):
             # call course constructor
@@ -422,7 +336,72 @@ class Account(models.Model):
         # wasn't sure if the private info should be its own method or not, so I did it like this
 
     def toString(self):
-        return "User ID: "+self.userID+"\nUsername: "+self.user.username+"\nUserEmail: " + \
-               self.user.userEmail+"\nUserPhone: "+self.userPhone+"\nUser Address: "+self.userAddress
+        return "User ID: " + self.userID + "\nUsername: " + self.user.username + "\nUserEmail: " + \
+               self.user.userEmail + "\nUserPhone: " + self.userPhone + "\nUser Address: " + self.userAddress
+    """
+
+#currently labs will not work
+class Lab(models.Model):
+    number = models.IntegerField(default=0)
+    ta = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 
+class Course(models.Model):
+    """
+    Courses Class: The class used to store course objects / edit / create them
+    """
+
+    # need to change later. Lab class? courses can have more than one lab and more than one ta
+    name = models.CharField(max_length=30)
+    number = models.IntegerField(default=0)
+    place = models.CharField(max_length=30)
+    days = models.CharField(max_length=30)
+    time = models.CharField(max_length=30)
+    semester = models.CharField(max_length=30)
+    professor = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='+')
+    ta = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='+')
+    #labs = models.ForeignKey(Lab, on_delete=models.CASCADE)
+
+    @classmethod
+    def create(cls, name, number, place, days, time, semester, professor, ta):
+        has_course = Course()
+        has_course = has_course.search(name)
+        if cls.professor.groupid > 2:
+            return "you do not have permission, contact your administrator or supervisor"
+        if has_course:
+            return "Course already exist"
+        else:
+            print("got into the else")
+            try:
+                accountp = Account.objects.filter(SignInName=professor)
+                accountp = accountp.first()
+            except Account.DoesNotExist:
+                accountp = None
+            try:
+                accountta = Account.objects.filter(SignInName=ta)
+                accountta = accountta.first()
+            except Account.DoesNotExist:
+                accountta = None
+
+            if accountp is not None:
+                if accountta is not None:
+                    course = cls(name=name, number=number, place=place, days=days, time=time, semester=semester,
+                                 professor=accountp, ta=accountta)
+                    course.save()
+                    return course
+            return "Course can not be created"
+
+    def search(self, name):
+        try:
+            check = Course.objects.get(name__contains=name)
+        except:
+            check = None
+        if check is not None:
+            return True
+        else:
+            return False
+
+    # call Course.objects.all() to get all courses to string
+    def __str__(self):
+        return str(self.name) + " " + str(self.number) + " " + str(self.place) + " " + str(self.days) + " " + str(
+            self.time) + " " + str(self.semester) + " " + str(self.professor) + " " + str(self.ta)
